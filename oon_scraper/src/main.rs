4bdbb8886fd691ae55ddb04853c9bab16460d345
Justinas Stankevicius <justinas@justinas.org>
2019-10-27 18:49:55 +0200

    Add a working scraper

--- oon_scraper/src/main.rs
+++ oon_scraper/src/main.rs
@@ -1,13 +1,38 @@
 mod types;
 
+use std::env;
+use std::sync::Arc;
+
+use oon_db::{models, Database};
+
+use types::*;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let page : types::Listing = reqwest::get("https://reddit.com/r/nottheonion.json")
+    let db_url = env::var("DATABASE_URL").expect("Expected DATABASE_URL to be set");
+    let db = Arc::new(Database::connect(&db_url)?);
+
+    let list: Listing = reqwest::get("https://reddit.com/r/theonion.json")
         .await?
         .json()
         .await?;
-    for post in page.data.children {
-        println!("{:?}", post.data.title);
-    }
+
+    let inserts = list.data.children.into_iter().map(|post| {
+        let db = db.clone();
+        tokio_executor::blocking::run(move || {
+            let id = uuid::Uuid::new_v4();
+            let question = models::NewQuestion {
+                id: &id,
+                foreign_id: &post.data.name,
+                title: &post.data.title,
+                url: &post.data.full_permalink(),
+                choice_id: 1,
+            };
+            db.insert_question(&question).unwrap();
+        })
+    });
+
+    futures::future::join_all(inserts).await;
+
     Ok(())
 }
--- oon_scraper/src/types.rs
+++ oon_scraper/src/types.rs
@@ -19,4 +19,12 @@ pub struct Post {
 #[derive(Deserialize, Debug)]
 pub struct PostData {
     pub title: String,
+    pub name: String,
+    pub permalink: String,
+}
+
+impl PostData {
+    pub fn full_permalink(&self) -> String {
+        format!("https://reddit.com{}", self.permalink)
+    }
 }
