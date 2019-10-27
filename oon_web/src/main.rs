1d69259db927599aef4d41214cc7546d454ce69f
Justinas Stankevicius <justinas@justinas.org>
2019-10-27 21:58:57 +0200

    Web backend PoC

---
+++ oon_web/src/main.rs
@@ -0,0 +1,56 @@
+mod types;
+
+use std::env;
+use std::sync::Arc;
+
+use warp::{path, Filter};
+
+use oon_db::{models, Database};
+
+use types::*;
+
+async fn get_question(db: Arc<Database>) -> Result<models::Question, oon_db::Error> {
+    tokio_executor::blocking::run(move || db.get_random_question()).await
+}
+
+async fn guess_handler(
+    db: Arc<Database>,
+    request: GuessRequest,
+) -> Result<impl warp::Reply, warp::reject::Rejection> {
+    let question = get_question(db)
+        .await
+        .map_err(|_| warp::reject::custom("oops"))?;
+
+    // TODO: report whether the answer is correct
+
+    Ok(warp::reply::json(&GuessResponse {
+        correct_choice_id: None,
+        your_choice_id: None,
+        next_question_id: question.id,
+        next_question_title: question.title,
+    }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = if let Ok(p) = env::var("PORT") {
+        p.parse()?
+    } else {
+        8000u16
+    };
+
+    let db_url = env::var("DATABASE_URL").expect("Expected DATABASE_URL to be set");
+    let db_arc = Arc::new(Database::connect(&db_url)?);
+
+    let base = warp::any()
+        .and(warp::body::content_length_limit(2 << 12))
+        .map(move || db_arc.clone());
+
+    let guess = base
+        .and(warp::path("guess"))
+        .and(warp::body::json::<GuessRequest>())
+        .and_then(guess_handler);
+
+    warp::serve(guess).run(([127u8, 0, 0, 1], port)).await;
+    Ok(())
+}
---
+++ oon_web/src/types.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct GuessRequest {
+    pub choice_id: Option<i32>,
+}
+
+#[derive(Serialize)]
+pub struct GuessResponse {
+    pub correct_choice_id: Option<i32>,
+    pub your_choice_id: Option<i32>,
+    pub next_question_id: uuid::Uuid,
+    pub next_question_title: String,
+}
