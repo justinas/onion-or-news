3bf7dd5c530716e406aec1919fda9b214bc92741
Justinas Stankevicius <justinas@justinas.org>
2019-10-28 18:27:42 +0200

    Render guess result

--- oon_web/static/script.js
+++ oon_web/static/script.js
@@ -14,8 +14,9 @@
   let app = new Vue({
     el: '#app',
     data: {
-      title: null,
+      result: null,
       question_id: null,
+      title: null,
     },
 
     created: function() {
@@ -29,16 +30,15 @@
       guess: function(choice_id) {
         sendGuess(this.question_id, choice_id)
           .then(d => {
+            this.result = {
+              correct: d.your_choice_id === d.correct_choice_id,
+              correct_choice_id: d.correct_choice_id,
+            };
             console.log(d);
-            if (d.your_choice_id == d.correct_choice_id) {
-              console.log('correct');
-            } else {
-              console.log('wrong');
-            }
             this.title = d.next_question_title;
             this.question_id = d.next_question_id;
           });
-      }
-    }
+      },
+    },
   });
 })();
