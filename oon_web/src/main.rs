03e4ba6e740214049077378033176f7cac596482
Justinas Stankevicius <justinas@justinas.org>
2019-10-28 19:53:41 +0200

    Record answers

--- oon_web/src/main.rs
+++ oon_web/src/main.rs
@@ -1,6 +1,7 @@
 mod types;
 
 use std::env;
+use std::net::SocketAddr;
 use std::sync::Arc;
 
 use warp::Filter;
@@ -20,9 +21,19 @@ async fn get_random_question(db: Arc<Database>) -> Result<models::Question, oon_
     tokio_executor::blocking::run(move || db.get_random_question()).await
 }
 
+async fn insert_answer(
+    db: Arc<Database>,
+    ip: std::net::IpAddr,
+    question_id: uuid::Uuid,
+    choice_id: i32,
+) -> Result<usize, oon_db::Error> {
+    tokio_executor::blocking::run(move || db.insert_answer(ip, question_id, choice_id)).await
+}
+
 async fn guess_handler(
     db: Arc<Database>,
     request: GuessRequest,
+    remote_addr: std::net::IpAddr,
 ) -> Result<impl warp::Reply, warp::reject::Rejection> {
     let this_question = match request.question_id {
         Some(id) => Some(
@@ -33,6 +44,13 @@ async fn guess_handler(
         None => None,
     };
     let this_question = this_question.as_ref();
+
+    if let (Some(qid), Some(cid)) = (this_question.map(|q| q.id), request.choice_id) {
+        let ip = remote_addr;
+        insert_answer(db.clone(), ip, qid.clone(), cid)
+            .await
+            .map_err(warp::reject::custom)?;
+    }
 
     let next_question = get_random_question(db)
         .await
@@ -48,6 +66,19 @@ async fn guess_handler(
         next_question_id: next_question.id,
         next_question_title: next_question.title,
     }))
+}
+
+/// Gets the remote client IP.
+///
+/// Note: the server MUST be behind a reverse proxy
+/// that sets the X-Forwarded-For header.
+/// Else, the client might spoof the header.
+///
+/// The fallback on remote socket address can be useful in development.
+fn get_ip() -> impl Filter<Extract = (std::net::IpAddr,), Error = warp::Rejection> + Clone {
+    warp::header("x-forwarded-for")
+        .or(warp::addr::remote().map(|a: Option<SocketAddr>| a.expect("No remote addr").ip()))
+        .unify()
 }
 
 #[tokio::main]
@@ -71,14 +102,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let guess = base
         .and(warp::path("guess"))
         .and(warp::body::json::<GuessRequest>())
+        .and(get_ip())
         .and_then(guess_handler);
 
-    let index = warp::path::end()
-        .and(warp::fs::file("static/index.html"));
+    let index = warp::path::end().and(warp::fs::file("static/index.html"));
 
-    let script = warp::path("script.js")
-        .and(warp::fs::file("static/script.js"));
+    let script = warp::path("script.js").and(warp::fs::file("static/script.js"));
 
-    warp::serve(index.or(script).or(guess)).run(([127u8, 0, 0, 1], port)).await;
+    warp::serve(index.or(script).or(guess))
+        .run(([127u8, 0, 0, 1], port))
+        .await;
     Ok(())
 }
